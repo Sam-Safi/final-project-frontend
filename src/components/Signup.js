a36790aa6d2601ddc214aaff02fa9538893174fe
sadegh <sadegh25_safikhani@yahoo.com>
2020-01-28 20:41:34 +1100

    login and signup files with form render created

---
+++ src/components/Signup.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+      errors: {}
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("User created: " + this.state.value);
+    event.preventDefault();
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Authorization", "Basic U2FtOmRvZG8=");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("firstname", "");
+    urlencoded.append("lastname", "");
+    urlencoded.append("email", "");
+    urlencoded.append("password", "");
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow"
+    };
+
+    fetch("/public/newUser", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log("error", error));
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      firstname: this.state.firstname,
+      lastname: this.state.lastname,
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    this.props.registerUser(newUser, this.props.history);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h3>Sign Up</h3>
+
+        <div className="form-group">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+            value={this.state.firstname}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Last name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Last name"
+            value={this.state.lastname}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary btn-block">
+          Sign Up
+        </button>
+        <p className="forgot-password text-right">
+          Already registered <a href="#">sign in?</a>
+        </p>
+      </form>
+    );
+  }
+}
